c216fcfef3a7ac532cc70432ed3f745d37f64996
Furgal <38505458+felipe-urgal@users.noreply.github.com>
2026-09-19 15:15:16 -0300

    style(web): load selected prototype theme

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -65,6 +65,7 @@ import './immersive-now-playing.css';
 import './immersive-now-playing-polish.css';
 import './folder-library-visual.css';
 import './now-playing-crossfade.css';
+import './prototype-one.css';
 import './tv.css';
 import './tv-login.css';
 import './tv-v2.css';
